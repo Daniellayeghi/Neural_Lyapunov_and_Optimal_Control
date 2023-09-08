57f83ecbd975550a3cec14d2286cfbab06d8d665
Daniel <Daniellayeghi@gmail.com>
2023-09-08 18:41:02 +0100

    Adaptive learning rate for seeds

--- CP_swingup.py
+++ CP_swingup.py
@@ -19,7 +19,7 @@ wandb.init(project='CP_swingup', anonymous="allow")
 torch.manual_seed(seed)
 sim_params = SimulationParams(6, 4, 2, 2, 1, 1, 200, 140, 0.01)
 cp_params = ModelParams(2, 2, 1, 4, 4)
-max_iter, max_time, alpha, dt, n_bins, discount, step, scale, mode = 121, 172, .5, 0.01, 3, 1, 15, 10, 'fwd'
+max_iter, max_time, alpha, dt, n_bins, discount, step, scale, mode = 300, 172, .5, 0.01, 3, 1, 15, 10, 'fwd'
 Q = torch.diag(torch.Tensor([.0, 0, .0, .0])).repeat(sim_params.nsim, 1, 1).to(device)
 R = torch.diag(torch.Tensor([0.0001])).repeat(sim_params.nsim, 1, 1).to(device)
 Qf = torch.diag(torch.Tensor([80, 600, .8, 4.5])).repeat(sim_params.nsim, 1, 1).to(device)
@@ -160,7 +160,10 @@ time_input = time.clone().reshape(time.shape[0], 1, 1, 1).repeat(1, sim_params.n
 one_step = torch.linspace(0, dt, 2).to(device)
 optimizer = torch.optim.AdamW(dyn_system.parameters(), lr=4e-3, amsgrad=True)
 
-lr = lr_scheduler.MultiStepLR(optimizer, milestones=[50, 60, 70, 80], gamma=0.5)
+from utilities.torch_utils import CustomLR
+custom_lr = CustomLR(optimizer=optimizer, halving_threshold=50)
+# lr = lr_scheduler.LambdaLR(optimizer, lr_lambda=custom_lr.lr_lambda)
+# lr = lr_scheduler.MultiStepLR(optimizer, milestones=[50, 60, 70, 80], gamma=0.5)
 lambdas = build_discounts(lambdas, discount).to(device)
 
 
@@ -174,7 +177,7 @@ if __name__ == "__main__":
             return param_group['lr']
 
     qc_init = torch.FloatTensor(sim_params.nsim, 1, 1).uniform_(0, 0) * 2
-    # Best under seed 4
+    # Best under seed
     qp_init = torch.FloatTensor(sim_params.nsim, 1, 1).uniform_(torch.pi - 0.3, torch.pi + 0.3)
     qp_init1 = torch.FloatTensor(sim_params.nsim//2, 1, 1).uniform_(torch.pi - 0.3, torch.pi)
     qp_init2 = torch.FloatTensor(sim_params.nsim//2, 1, 1).uniform_(torch.pi, torch.pi + 0.3)
@@ -196,10 +199,12 @@ if __name__ == "__main__":
             time = torch.linspace(0, (sim_params.ntime - 1) * dt, sim_params.ntime).to(device).requires_grad_(True)
             time_input = time.clone().reshape(time.shape[0], 1, 1, 1).repeat(1, sim_params.nsim, 1, 1).requires_grad_(True)
 
-        optimizer.step()
-        lr.step(iteration)
-        wandb.log({'epoch': iteration+1, 'loss': loss.item()})
         print(f"Epochs: {iteration}, Loss: {loss.item()}, lr: {get_lr(optimizer)}, T: {sim_params.ntime}, Total time steps: {total_time_steps}, Update: {update}\n")
+        wandb.log({'epoch': iteration+1, 'loss': loss.item()})
+
+        optimizer.step()
+        custom_lr.step(loss.item())
+        # lr.step(iteration)
 
         if iteration % 20 == 0:
             indices = torch.randperm(sim_params.nsim-1 - 0)[:10]
--- utilities/torch_utils.py
+++ utilities/torch_utils.py
@@ -1,24 +1,29 @@
-from torch.optim.lr_scheduler import _LRScheduler
 
-
-class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, halving_threshold, step_interval=10, last_epoch=-1):
-        self.halving_threshold = halving_threshold
-        self.step_interval = step_interval
+class CustomLR:
+    def __init__(self, optimizer,  halving_threshold, step_interval=10):
         self.steps_since_halving = 0
         self.should_halve = False
-        super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
+        self.halving_threshold = halving_threshold
+        self.step_interval = step_interval
+        self._not_halved = True
+        self.optimizer = optimizer
 
     def get_lr(self):
+        for param_group in self.optimizer.param_groups:
+            return param_group['lr']
+
+    def set_lr(self, lr):
+        for param_group in self.optimizer.param_groups:
+            param_group['lr'] = lr
+
+    def lr_lambda(self, epoch):
         if self.should_halve:
             self.steps_since_halving += 1
-            if self.steps_since_halving % self.step_interval == 0:
-                return [group['lr'] / 2.0 for group in self.optimizer.param_groups]
-
-        return [group['lr'] for group in self.optimizer.param_groups]
+            if self.steps_since_halving % self.step_interval == 0 or self._not_halved:
+                self.set_lr(self.get_lr() * 0.5)
+                self._not_halved = False
 
     def step(self, loss):
         if loss <= self.halving_threshold:
             self.should_halve = True
-
-        super(CustomLRScheduler, self).step()
+            self.lr_lambda(None)
